5c9c587bf62a1b58852ae41e9d3f3f6033d3522c
Anton Zemel <sunny.warcraft@gmail.com>
2021-03-11 21:56:19 +0600

    Abstract 4 HW12

--- Abstract-5/shapes1.js
+++ Abstract-5/shapes1.js
@@ -1,6 +1,9 @@
 const logRectangle = rectangle => 
   console.log("Прямоугольник: " + rectangle.width + ", " + rectangle.height);
 
+const logSquare = square => 
+  console.log("Квадрат: " + square.width);
+
 const logCircle = circle => 
   console.log("Круг: " + circle.radius)
 
@@ -9,62 +12,187 @@ const logTriangle = triangle =>
 
 
 
-  
-const constructRectangle = (width, height) => ({
-  $type: "rectangle",
-  width,
-  height,
-  perimeter: function() {
-    return (this.width + this.height) * 2;
-  },
-  log: function() {
-    logRectangle(this);
+function Rectangle(width, height) {
+  this.width = width;
+  this.height = height;
+};
+
+Rectangle.prototype.$type = "rectangle";
+
+Rectangle.prototype.perimeter = function() {
+  return (this.width + this.height) * 2;
+};
+
+Rectangle.prototype.log = function() {
+  logRectangle(this);
+};
+
+
+// "Статические функции" static
+Rectangle.comaparePerimeters = (rect1, rect2) => 
+  rect1.perimeter() - rect2.perimeter();
+
+
+function Square(width) {
+  this.width = width;
+  this.height = width;
+}
+
+Square.prototype.$type = "square";
+
+Square.prototype.log = function() {
+  logSquare(this);
+};
+
+// "Наследование"
+Object.setPrototypeOf(Square.prototype, Rectangle.prototype);
+
+// Square -> Rectangle -> (Object);
+
+
+
+// Доступность свойств/методов
+// в ООП: private, public, (protected)
+function Circle(name, radius) {
+  // this.name = name;
+  this.radius = radius;  
+
+  this.log = function() {
+    console.log("Круг " + name + ": " + this.radius)
   }
-});
+
+  this.getName = function() {
+    return name;
+  }
+
+  this.setName = function(newName) {
+    if (newName && typeof newName === "string") {
+      name = newName;
+    }
+  }
+};
+
+Circle.prototype.$type = "circle";
+
+Circle.prototype.perimeter = function() {
+  return 2 * Math.PI * this.radius;
+};
+
+Circle.prototype.log = function() {
+  logCircle(this);
+};
+
+Circle.comaparePerimeters = (circle1, circle2) => 
+  circle1.perimeter() - circle2.perimeter();
 
 
-const constructCircle = (radius) => ({
-  $type: "circle",
-  radius,
-  perimeter: function() {
-    return 2 * Math.PI * this.radius;
-  },
-  log: function() {
-    logCircle(this);
-  },
-});
 
+function Triangle(a, b, c) {
+  this.a = a;
+  this.b = b;
+  this.c = c;
+};
 
-const constructTriangle = (a, b, c) => ({
-  $type: "triangle",
-  a,
-  b,
-  c,
-  perimeter: function() {
-    return this.a + this.b + this.c;
-  },
-  log: function() {
-    logTriangle(this);
-  },
-});
+Triangle.prototype.$type = "triangle";
+
+Triangle.prototype.perimeter = function() {
+  return this.a + this.b + this.c;
+}
+
+Triangle.prototype.log = function() {
+  logTriangle(this);
+}
 
 
 const shapes = [
-  constructRectangle(100, 50),
-  constructRectangle(150, 20), 
-  constructCircle(100), 
-  constructCircle(200), 
-  constructTriangle(100, 150, 200), 
-  constructTriangle(300, 400, 500)
+  new Rectangle(100, 50),
+  new Rectangle(150, 20), 
+  new Square(100),
+  new Square(150), 
+  new Circle("Первый", 100), 
+  new Circle("Красный", 200), 
+  new Triangle(100, 150, 200), 
+  new Triangle(300, 400, 500)
 ];
 console.log(shapes);
 
+console.log("--------");
 
 shapes.forEach(shape => {
-  switch (shape.$type) {
-    case "rectangle": logRectangle(shape); break;
-    case "circle": logCircle(shape); break;
-    case "triangle": logTriangle(shape); break;
-    default: console.log("Неизвестная фигура");
+
+  if (shape instanceof Square) {
+    logSquare(shape);
+  } else if (shape instanceof Rectangle) {
+    logRectangle(shape);
+  } else if (shape instanceof Circle) {
+    logCircle(shape);
+  } else if (shape instanceof Triangle) {
+    logTriangle(shape);
+  } else {
+    console.log("Неизвестная фигура");
   }
 });
+
+console.log("--------");
+
+const circle = shapes[4];
+console.log(circle.getName());
+circle.setName("Последний");
+circle.setName(123);
+console.log(circle.getName());
+
+shapes.forEach(shape => shape.log());
+
+console.log(shapes.map(shape => shape.perimeter()));
+
+console.log(shapes[0].log === shapes[1].log);
+
+
+const rectangle = shapes[0];
+
+rectangle.width = 300;
+
+rectangle.log();
+console.log(rectangle, rectangle.perimeter());
+
+
+rectangle.name = "Специальный прямоугольник";
+rectangle.price = 1000;
+
+console.log(rectangle);
+
+Rectangle.prototype.log = function () {
+  console.log(this.name + ": " + this.width + ", " + this.height);
+}
+
+console.log(rectangle);
+
+rectangle.log();
+rectangle.comaparePerimeters();
+
+console.log("-----")
+shapes.forEach(shape => shape.log());
+
+
+const getDiagonalOfRectangle = rectangle => {
+  if (rectangle instanceof Rectangle) {
+    return Math.sqrt(rectangle.width * rectangle.width + rectangle.height * rectangle.height);
+  }
+
+  console.log("Не прямоугольник!");
+  return null;
+}
+
+
+console.log(getDiagonalOfRectangle(shapes[0]));
+console.log(getDiagonalOfRectangle(shapes[1]));
+console.log(getDiagonalOfRectangle(shapes[2]));
+console.log(getDiagonalOfRectangle(shapes[3]));
+
+
+
+const rectangles = shapes.filter(shape => shape instanceof Rectangle);
+console.log(rectangles);
+
+rectangles.sort(Rectangle.comaparePerimeters);
+console.log(rectangles);
